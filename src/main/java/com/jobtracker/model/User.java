6b1d107df8c3ecc987cddc03b5e17fb00b366a85
sanjay123-321 <sanjayup21@gmail.com>
2026-06-26 16:55:27 -0400

    Develop: Added the repository code

--- src/main/java/com/jobtracker/model/User.java
+++ src/main/java/com/jobtracker/model/User.java
@@ -1,4 +1,19 @@
 package com.jobtracker.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String firstName;
+    private String lastName;
+
 }
